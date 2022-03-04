9382ae7baee87a481b5a7a1d5caaa957cadb140e
Lochlann ONeill <l.carroll-oneill@mycit.ie>
2022-03-04 03:23:23 +0000

    working on persistent login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import './App.css';
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { selectUser } from './features/userSlice';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
 import Feed from './Feed';
 import Header from './Header';
 import Login from './Login';
 import Sidebar from './Sidebar'; 
+import { auth } from './firebase';
 
 
 function App() {
   const user = useSelector(selectUser)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    auth.onAuthStateChanged(userAuth => {
+      if (userAuth) {
+        //user is logged in
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+            displayName: userAuth.displayName,
+          })
+        )
+      } else {
+        //user is logged out
+         dispatch(logout());
+      }
+    })
+  })
 
   return (
     <div className="app">
